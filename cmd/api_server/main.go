099bdb57c62796be99bdfa6727bf232ca5097de3
BulatKarimov <bulatkarimov96@gmail.com>
2021-12-16 13:12:09 +0300

    [ref] main

--- cmd/api_server/main.go
+++ cmd/api_server/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"github.com/BulatKarimov/gin_rest_api/internal/api_server/controllers"
 	"github.com/BulatKarimov/gin_rest_api/internal/api_server/models"
 	"github.com/BulatKarimov/gin_rest_api/internal/api_server/router"
 	"github.com/gin-gonic/gin"
 )
-
-func initializeRoutes(r *gin.Engine) {
-	r.GET("/events", controllers.FindEvents)
-	r.POST("/events", controllers.CreateEvent)
-}
 
 func main() {
 	r := gin.Default()
